0e286729c27581cfcd082f56da16d78c15625c49
Your Name <you@example.com>
2025-08-22 22:59:40 +0300

    edits

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request, redirect, url_for, session, flash
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask import send_from_directory
+
 import os
 
 # Initialize app
